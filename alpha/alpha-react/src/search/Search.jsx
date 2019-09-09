75971b699aacc3ff6df5bbed7fd628b6ba762955
Austin Mleziva <AustinMleziva@gmail.com>
2019-09-09 10:42:24 -0500

    Adding history update to search button click

--- alpha/alpha-react/src/search/Search.jsx
+++ alpha/alpha-react/src/search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component, useState} from 'react';
 import PropTypes from 'prop-types';
 import { Input, Button, List, Container} from 'semantic-ui-react';
 import config from '.././.config.secret';
@@ -7,13 +7,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 export class Search extends Component {
+  
   constructor(props) {
     super(props);
-    this.state = { returnedValues: null, failed: null, resourceUrl: config.resourceServer.apiSampleUrl };
-
-    
+    this.state = {
+      searhValue: ''
+    };
   }
   componentDidMount() {
+    console.log(this.props);
     this.props.action.getSearchAction()
     .catch(error => {
         console.log(error);
@@ -21,14 +23,12 @@ export class Search extends Component {
     });
 }
 
+
   render() {
     const { results } = this.props;
-    console.log("prior to logging results in jsx");
-    console.log(results);
     return (
       <div>
-        <Input placeholder='What are you looking for...' />
-        <Button primary>Search</Button>
+        <SearchArea history={this.props.history} location={this.props.location}/>
         <Container>
           <List>
             {results.map((thing) =>(<List.Item>{thing.firstName} {thing.lastName}</List.Item>))}
@@ -37,7 +37,30 @@ export class Search extends Component {
       </div>
     );
   }
+
+  
+
 };
+
+function SearchArea(props) {
+  const [search, setSearch] = useState('');
+  const handleSearch = (event) => {
+    //do updating the url here and execute search api request
+    //if search is the same do nothing
+    let queryString = "?q="+search;
+    console.log(props);
+    if(props.location.search !== queryString){
+      props.history.push("/search"+queryString);
+    }
+  };
+  return (
+    <div>
+      <p>You searched for {search}</p>
+      <Input placeholder='What are you looking for...' onInput={e => setSearch(e.target.value)}  />
+      <Button primary onClick={handleSearch}>Search</Button>
+    </div>
+  );
+}
 
 const mapStateToProps = state => ({
     results: state.searchReducer.results

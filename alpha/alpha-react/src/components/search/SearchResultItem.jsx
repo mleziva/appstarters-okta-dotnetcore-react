f23e247dee16b6dc4496e21d376ab31436d0c970
Austin Mleziva <AustinMleziva@gmail.com>
2019-09-11 08:34:47 -0500

    Aligning search results item button right

--- alpha/alpha-react/src/components/search/SearchResultItem.jsx
+++ alpha/alpha-react/src/components/search/SearchResultItem.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import SearchResult from './SearchResult'
-import { Segment} from 'semantic-ui-react';
+import { Segment, Button, Grid} from 'semantic-ui-react';
 
 const SearchResultItem = ({ searchResult, onActionToPerform }) => (
   <div style={{ marginBottom: 20 }}>
     <Segment>
-    <SearchResult
+      <Grid columns={2}>
+          <Grid.Column width={12}>
+          <SearchResult
       title={searchResult.title}
       price={searchResult.price}
       quantity={searchResult.inventory} />
-    <button
+          </Grid.Column>
+          <Grid.Column width={4} className="search-result-item btn-grid">
+          <Button 
       onClick={onActionToPerform}
       disabled={searchResult.inventory > 0 ? '' : 'disabled'}>
       {searchResult.inventory > 0 ? 'Add to cart' : 'Sold Out'}
-    </button>
+    </Button>
+            </Grid.Column>
+      </Grid>
     </Segment>
   </div>
 )
